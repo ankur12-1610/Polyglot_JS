ee5614c9100663a9dccade795eb7bdcc46c95dcd
shubhangi013 <choudharyshubhangi13@gmail.com>
2022-02-02 03:07:42 +0530

    added the instructions

---
+++ main.js
@@ -0,0 +1,124 @@
+// HELLO FELLOW CODERS! WE WELCOME YOU TO THE WORLD OF JAVASCRIPT 
+
+//----- We've curated this assignment for someone staring out in exploring the beauty of JavaScript and would urge you to go through the resources shared with you before you start with this. ----- // 
+
+//Go through the CSS file as well to get a hold of all the attributes we've added. You're free to add some of your own and maybe delete some of ours xD
+
+//The point is, we want you to have fun and use all the concepts while doing this task. In case of any doubts, feel free to reach out to us!
+
+// Your main work would be here, in main.js and would advice you to also pay a visit to the scenario.js
+
+
+// Life of the player and the hacker.
+var playerLife = 5;
+var hackerLife = 5;
+
+// Message to be displayed when the game is over
+var hackerWinnerMessage = "Write the message here";
+var playerWinnerMessage = "Write the message here";
+
+          // ---------------Game code starts here ---------------//
+
+// declare a few handy variables like we've done :p
+
+var playerStartLife = parseInt(playerLife);
+var hackerStartLife = parseInt(hackerLife);
+
+// we will declare the functions for you and you will complete those 
+updateScores();
+
+// you learnt DOM manipulation right? here's an example of the same. Go ahead and use manipulate the DOM!
+document.querySelector(".game-board").classList.add("before-game");
+
+var allCardElements = document.querySelectorAll(".card");
+
+// Adds click handler to all player card elements so that your cards are actionable
+
+
+// An example of a function that controls what would happen when a card is clicked
+
+function cardClicked(cardEl) {
+
+  if(cardSelected) { return; }
+  cardSelected = true;
+
+  cardEl.classList.add("played-card");
+
+  document.querySelector(".game-board").classList.add("card-selected");
+
+  // Yes JS is cool and this would allow you to wait 500ms beforing revealing the hacker power
+  setTimeout(function(){
+    revealHackerPower();
+  },500)
+
+  setTimeout(function(){
+    revealPlayerPower();
+  },800)
+  
+  setTimeout(function(){
+    compareCards();
+  }, 1400);
+}
+
+// Now write a function that shows the power level on the player card
+function revealPlayerPower(){
+  
+}
+
+// Write a function that shows the power level on the hacker card
+function revealHackerPower(){
+
+}
+// Write a function to compare the cards. Here is where all your skills would come in handy! 
+// P.S: We've added the 'disabled' attribute in the CSS file for the button and you should use it in case you want a certain element to just go away or 'vanish' at a certain  time. For eg: You'd definitely want the 'Next' button to go away after a player chooses a card right?
+
+function compareCards(){
+
+}
+
+//Use conditional statements and complete the fucnbtion that shows the winner message
+function gameOver(winner) {
+  
+}
+
+
+// Write a function that starts the game
+function startGame() {
+
+}
+
+
+// Now write a function that starts the game over from scratch
+function restartGame(){
+
+}
+
+// We've also used a cool life bar that displays the life left. Write a function that updates the displayed life bar and life totals
+// use innerHTML and a lot of other cool things to do this. 
+function updateScores(){
+
+  // Here these update life totals for each player
+  document.querySelector(".player-stats .life-total").innerHTML = playerLife;
+
+  // We've added the code to update the player lifebar
+  var playerPercent = playerLife / playerStartLife * 100;
+  if (playerPercent < 0) {
+    playerPercent = 0;
+  }
+  document.querySelector(".player-stats .life-left").style.height =  playerPercent + "%";
+
+  // Now you write the code to update the hacker lifebar
+
+}
+
+
+
+// Write a function that Plays one turn of the game
+function playTurn() {
+
+}
+
+// Finally write the function that reveals the cards. Use 
+function revealCards(){
+
+}
---
+++ scenario.js
@@ -0,0 +1,51 @@
+// Add here all the scenarios you can think of!
+
+// The only condition being : add 3 playerCard description for 1 hackerCard Situation. We've already added a few as an example.
+
+// P.S.: Keep in mind the Power of the number so the witty players don't end up losing :p
+
+// How 'power' works : write a power for the hacker's card and while you write powers for the player's cards, keep in mind that the power of the correct counters should be greater than the hacker's card's power. 
+
+var scenarios = [
+  { // add the text you'd want should appear on the hacker's card
+    hackerCard : {
+      description : "I set up a fake Wi-Fi station to steal people’s email and track them online.",
+      power : 4,
+    },
+    // add 3 card descriptions you'd want should appear on the player's card. Keeping in mind that at least ONE of them should be an apt counter!
+    playerCards : [
+      {
+        description : "I never use public wifi networks.",
+        power : 5,
+      },
+      {
+        description : "I browse the web, but I never do any personal business on a public wifi network.",
+        power : 3,
+      },
+      {
+        description : "I connect to any wifi network I can use in public.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "I sent a fake email from your bank asking for your account details.",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I checked the email address - the message didn’t come from my bank.",
+        power : 5,
+      },
+      {
+        description : "I never give out personal information in response to an email.",
+        power : 4,
+      },
+      {
+        description : "I sent the details you asked for so you could check on my account.",
+        power : 1,
+      }
+    ]
+  },
+];

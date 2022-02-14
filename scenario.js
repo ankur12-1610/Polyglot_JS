3ec8c263f666e0f0c22ffbbd83e578a08364c738
taranimishra25.name <tarani.mishra.eee21@itbhu.ac.in>
2022-02-14 13:58:07 +0530

    FINAL CHANGES

--- scenario.js
+++ scenario.js
@@ -6,46 +6,100 @@
 
 // How 'power' works : write a power for the hacker's card and while you write powers for the player's cards, keep in mind that the power of the correct counters should be greater than the hacker's card's power. 
 
-var scenarios = [
-  { // add the text you'd want should appear on the hacker's card
-    hackerCard : {
-      description : "I set up a fake Wi-Fi station to steal people’s email and track them online.",
-      power : 4,
-    },
-    // add 3 card descriptions you'd want should appear on the player's card. Keeping in mind that at least ONE of them should be an apt counter!
-    playerCards : [
-      {
-        description : "I never use public wifi networks.",
-        power : 5,
-      },
-      {
-        description : "I browse the web, but I never do any personal business on a public wifi network.",
-        power : 3,
-      },
-      {
-        description : "I connect to any wifi network I can use in public.",
-        power : 1,
-      }
-    ]
+var scenarios = [{ // add the text you'd want should appear on the hacker's card
+  hackerCard: {
+      description: "I sent you a fake link in mail from unknown website for the purpose of phishing.",
+      power: 3,
   },
-  {
-    hackerCard : {
-      description : "I sent a fake email from your bank asking for your account details.",
-      power : 3,
-    },
-    playerCards : [
-      {
-        description : "I checked the email address - the message didn’t come from my bank.",
-        power : 5,
+  // add 3 card descriptions you'd want should appear on the player's card. Keeping in mind that at least ONE of them should be an apt counter!
+  playerCards: [{
+          description: "I avoid clicking on any unknown links.",
+          power: 5,
       },
       {
-        description : "I never give out personal information in response to an email.",
-        power : 4,
+          description: "Ohh Shit!! I clicked that fake link.",
+          power: 1,
       },
       {
-        description : "I sent the details you asked for so you could check on my account.",
-        power : 1,
+          description: "I will try to first verfiy the link from people I know.",
+          power: 3,
       }
-    ]
+  ]
+},
+{
+  hackerCard: {
+      description: "I set up a fake Wi-Fi station to steal people’s email and track them online.",
+      power: 4,
   },
+  // add 3 card descriptions you'd want should appear on the player's card. Keeping in mind that at least ONE of them should be an apt counter!
+  playerCards: [{
+          description: "I never use public wifi networks.",
+          power: 5,
+      },
+      {
+          description: "I browse the web, but I never do any personal business on a public wifi network.",
+          power: 3,
+      },
+      {
+          description: "I connect to any wifi network I can use in public.",
+          power: 1,
+      }
+  ]
+},
+{
+  hackerCard: {
+      description: "I sent a fake email from your bank asking for your account details.",
+      power: 3,
+  },
+  playerCards: [{
+          description: "I checked the email address - the message didn’t come from my bank.",
+          power: 5,
+      },
+      {
+          description: "I never give out personal information in response to an email.",
+          power: 4,
+      },
+      {
+          description: "I sent the details you asked for so you could check on my account.",
+          power: 1,
+      }
+  ]
+}, { // add the text you'd want should appear on the hacker's card
+  hackerCard: {
+      description: "I have your official email password.",
+      power: 3,
+  },
+  // add 3 card descriptions you'd want should appear on the player's card. Keeping in mind that at least ONE of them should be an apt counter!
+  playerCards: [{
+          description: "I frequently change my password but not sure whether did it this time.",
+          power: 4,
+      },
+      {
+          description: "I didn't change the password.",
+          power: 3,
+      },
+      {
+          description: "I clearly remember that I changed my password so no worries .",
+          power: 5,
+      }
+  ]
+}, {
+  hackerCard: {
+      description: "I want your aadhar card details.",
+      power: 3,
+  },
+  playerCards: [{
+          description: "I never share my aaadhar card details.",
+          power: 5,
+      },
+      {
+          description: "I will first contact my friends to confirm about it.",
+          power: 4,
+      },
+      {
+          description: "I will share my aadhar card details with you.",
+          power: 1,
+      }
+  ]
+}
 ];
